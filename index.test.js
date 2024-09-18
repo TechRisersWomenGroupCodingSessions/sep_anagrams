6c4d4408a5480a4fb8202aff927245657b20fe90
Cornelia P. <92683838+gros-pataplouf@users.noreply.github.com>
2024-09-18 18:12:50 +0000

    on red: try reading file

--- index.test.js
+++ index.test.js
@@ -1,4 +1,18 @@
+const fs = require('node:fs');
 const is_anagram = require('./index');
+
+
+async function getWords() {
+    const retrievedWords = await fs.readFile('words.txt', 'utf8', (err, data) => {
+        if (err) {
+          console.error("got error", err);
+          return;
+        }
+        console.log("no error", data)
+        return data;
+      });
+} 
+
 
 test('is anagram', () => {
     expect(is_anagram("kinship", "pinkish")).toBe(true);
@@ -11,3 +25,12 @@ test('is not anagram due to length', () => {
 test('is not anagram due to different char', () => {
     expect(is_anagram("kinship", "pinkisx")).toBe(false);
 });
+
+test('can retrieve the word list', async () => {
+    const myWords = await getWords();
+    expect(myWords.toBeDefined());
+
+}
+
+)
+
