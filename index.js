7a70ad4f0e0d54f6ce2493af75d1b857a6c7f81d
Sarah Usher <sarahnusher@gmail.com>
2024-09-18 18:53:53 +0100

    not anagram length passes

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ function is_anagram(word1, word2) {
     if (word1.length != word2.length) {
         return false
     }
-    
-    return true
+
+    const word1Arr = Array.from(word1).toSorted()
+    const word2Arr = Array.from(word2).toSorted()
+
+    const word1Sorted = word1Arr.toString()
+    const word2Sorted = word2Arr.toString()
+
+    if (word1Sorted == word2Sorted){
+        return true
+    }
+    else{
+    return false
+    }
+
 }
 
 module.exports = is_anagram;
